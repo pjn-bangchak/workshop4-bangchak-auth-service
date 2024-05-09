9c03ba553d416a2df79eb0655854820120c98a54
Akenarin Komkoon <codingthailand@gmail.com>
2024-05-09 10:45:25 +0700

    update

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,7 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using BangchakAuthService.Areas.Identity.Data;
 using BangchakAuthService.ModelsDto;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BangchakAuthService.Controllers;
 
@@ -11,10 +15,12 @@ public class AuthController : ControllerBase
 {
     private readonly UserManager<User> _userManager;
     private readonly SignInManager<User> _signInManager;
+    private readonly IConfiguration _configuration;
 
-    public AuthController(UserManager<User> userManager, SignInManager<User> signInManager) {
+    public AuthController(IConfiguration configuration ,UserManager<User> userManager, SignInManager<User> signInManager) {
         _userManager = userManager;
         _signInManager = signInManager;
+        _configuration = configuration;
     }
 
     // localhost:port/api/v1/Auth/Home
@@ -57,4 +63,48 @@ public class AuthController : ControllerBase
 
         return Ok(new {message = "เข้าระบบสำเร็จ แล้วสร้าง token"});
     }
+
+            // create JWT token
+        private async Task<IActionResult> CreateToken(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null) return NotFound();
+
+            // เตรียม payload สำหรับสร้าง token
+            var payload = new List<Claim>
+            {
+                new("userId", user.Id),
+                new(JwtRegisteredClaimNames.Email, user.Email!)
+            };
+
+            // เพิ่ม role ของ user แต่ละคนเข้าไปใน payload ด้วย (role-base authentication)
+            // var userRoles = await _userManager.GetRolesAsync(user);
+            // if (userRoles != null)
+            // {
+            //     payload.AddRange(userRoles.Select(role => new Claim(ClaimsIdentity.DefaultRoleClaimType, role)));
+            // }
+
+            // ดึง JWT_KEY จาก appsettings.json
+            var jwtKey = Encoding.UTF8.GetBytes(_configuration.GetSection("JWT_KEY").Value!);
+
+            // เตรียมข้อมูล payload token / Algorithms / expire 
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(payload),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(jwtKey), SecurityAlgorithms.HmacSha256),
+                Expires = DateTime.UtcNow.AddDays(7)
+            };
+
+            // สร้าง token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                access_token = tokenHandler.WriteToken(token),
+                expiration = token.ValidTo
+            });
+
+        }
+
 }
