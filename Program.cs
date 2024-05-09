d4511ab5c18a8d480a3e07e694aab7160b1ad122
Akenarin Komkoon <codingthailand@gmail.com>
2024-05-09 11:51:26 +0700

    update

--- Program.cs
+++ Program.cs
@@ -1,15 +1,45 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using BangchakAuthService.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("BangchakAuthServiceIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'BangchakAuthServiceIdentityDbContextConnection' not found.");
 
+// Add Cors Service
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(policy => {
+        policy.AllowAnyOrigin(); // policy.WithOrigins("https://codingthailand.com")
+        policy.AllowAnyMethod();
+        policy.AllowAnyHeader();
+    });
+});
+
+var connectionString = builder.Configuration.GetConnectionString("BangchakAuthServiceIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'BangchakAuthServiceIdentityDbContextConnection' not found.");
 builder.Services.AddDbContext<BangchakAuthServiceIdentityDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
 .AddEntityFrameworkStores<BangchakAuthServiceIdentityDbContext>();
 
 // Add services to the container.
+// add Jwt service
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+                config =>
+                {
+                    //config.SaveToken = true;
+
+                    config.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWT_KEY").Value!)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                        ClockSkew = TimeSpan.Zero
+                    };
+
+                    
+                }
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,6 +57,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors();
 
 app.UseAuthentication();
 app.UseAuthorization();
